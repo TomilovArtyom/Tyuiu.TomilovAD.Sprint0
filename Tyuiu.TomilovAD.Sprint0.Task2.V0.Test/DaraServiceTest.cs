dde49a8d9c8bb314d2975413a8c60aad799c7bc4
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-09-18 11:55:12 +0500

    Task2

---
+++ Tyuiu.TomilovAD.Sprint0.Task2.V0.Test/DaraServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.TomilovAD.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DaraServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Артём";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Артём", res);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TomilovAD.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TomilovAD.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41bdfc1f-33a2-4a68-9e26-7e675b0395dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
